8fa37cac6bb9367f11c85771fd078673d51de1e0
Najkloo <mateuszgornicki3@gmail.com>
2023-02-20 14:25:59 +0100

    Zadanie z js1

--- Zadanie 3/zad3.js
+++ Zadanie 3/zad3.js
@@ -1,19 +1,20 @@
-
+//1
 const jedynka = document.querySelector("#buton");
 jedynka.addEventListener("click", function() {
     document.querySelector("#szesc").innerHTML = "<i>Świetnie</i>";
 });
 console.log(jedynka);
-
+//2
 const dwojka = document.querySelector("#piaty");
 dwojka.addEventListener("click", function() {
     document.querySelector("#piec").innerHTML = "<i>Świetnie</i>";
 });
 console.log(dwojka);
 
-
+//3
 const trzy = document.querySelector("#buton1");
 trzy.addEventListener("click", function() {
-    const trzy_ele = document.querySelector("#trio");
+    const trzy_ele = document.querySelector("#trio strong");
+    trzy.style.border='2px dotted red';
 console.log(trzy_ele);
 });
