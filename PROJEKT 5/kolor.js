ccdd27951bd5167f5d9db584e373fddf639b0070
Najkloo <mateuszgornicki3@gmail.com>
2023-04-03 15:25:50 +0200

    ZADANIA JAVA 3

--- PROJEKT 5/kolor.js
+++ PROJEKT 5/kolor.js
@@ -4,10 +4,12 @@ const div2 = document.getElementById('cztery');
 const div3 = document.getElementById('siedem');
 checkbox.addEventListener("change", function (){
     if(this.checked) {
+        console.log('dziala')
         div.classList.remove("hide_number");
         div2.classList.remove("hide_number");
         div3.classList.remove("hide_number");
     } else {
+        console.log('niedziala')
         div.classList.add("hide_number");
         div2.classList.add("hide_number");
         div3.classList.add("hide_number");
