81658dfe15685dfd9dfd178ec0bf2c654107e378
Najkloo <mateuszgornicki3@gmail.com>
2023-04-24 15:32:51 +0200

    ZADANIA JAVA 4

---
+++ PROJEKT 24.04/projekt24.js
@@ -0,0 +1,16 @@
+let a = Math.abs(-6.5);
+let a2 = Math.abs(4.3);
+
+let b = Math.ceil(-6.5);
+let b2 = Math.ceil(4.3);
+
+let c = Math.floor(-6.5);
+let c2 = Math.floor(4.3);
+
+let d = Math.max(-6.5, 4.3);
+let d2 = Math.min(-6.5, 4.3);
+
+let e = Math.pow(-6.5);
+let e2 = Math.pow(4.3);
+document.getElementById('pierwszy')
+
