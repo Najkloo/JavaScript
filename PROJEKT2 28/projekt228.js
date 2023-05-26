4e321de24576a58f760382ba804fcaae09aa76b7
Najkloo <mateuszgornicki3@gmail.com>
2023-05-26 09:24:55 +0200

    ZADANIA 28.04 5

--- PROJEKT2 28/projekt228.js
+++ PROJEKT2 28/projekt228.js
@@ -1,3 +1,3 @@
 const przedzial = prompt("Podaj pierwsza liczbe");
 const przedzial2 = prompt("Podaj druga liczbe");
-document.getElementById('pierwsza').innerText = ("Dziesięć losowych liczb całkowitch z przedziału od " + przedzial + przedzial2);
+document.getElementById('pierwsza').innerText = ("Dziesięć losowych liczb całkowitch z przedziału od " + przedzial + przedzial2); kutas
